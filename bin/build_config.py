4ffe379ab0bf6630413c4c397370581eea30f19b
Gwang Yoon Hwang <yoon@igalia.com>
2016-11-22 17:39:25 +0900

    [WebKit] Modify webkit script to build all possible build options at once.
    And it has some additional clean up for llvm script too.

--- bin/build_config.py
+++ bin/build_config.py
@@ -1,7 +1,8 @@
-
+import argparse
+import itertools
 import json
-import os.path
 import os
+import os.path
 import sys
 
 class ConfigHelper:
@@ -10,6 +11,43 @@ class ConfigHelper:
     with open(self.config_file, 'w') as configFile:
       print "writing to config file"
       json.dump(self.config, configFile, sort_keys=True, indent=4, separators=(',', ': '))
+
+  def get_webkit_dir(self, options):
+    if options.debug:
+      name = 'Debug'
+    elif not options.debug:
+      name = 'Release'
+
+    if options.use_gles:
+      name = name + 'GLES'
+    if options.disable_threaded_compositor:
+      name = name + 'NoTC'
+    if options.disable_gstgl:
+      name = name + 'NoGstGL'
+
+    return os.path.join(self.build_dir, name + '/')
+
+  def get_argument_parser(self):
+    argParser = argparse.ArgumentParser(description='Webkit dev script using custom jhbuild env')
+    argParser.add_argument('--release', action='store_false', dest='debug', default=False, help='Compile with Debug configuration (default: Release)')
+    argParser.add_argument('--debug', action='store_true', default=False, help='Compile with Debug configuration (default: Release)')
+    argParser.add_argument('--install', action='store_true', default=False, help='Install Webkit to jhbuild')
+    argParser.add_argument('--all', action='store_true', default=False, help='Build WebKit with all possible configurations (default: False)')
+
+    argParser.add_argument('--disable-threaded-compositor', action='store_true', dest='disable_threaded_compositor', default=False, help='Disable Threaded Compositor (default: False)')
+    argParser.add_argument('--disable-gst-gl', action='store_true', dest='disable_gstgl', default=False, help='Disable Gst GL (default: False)')
+    argParser.add_argument('--use-opengles', action='store_true', dest='use_gles', default=False, help='Use OpenGLES (default: False)')
+
+    argParser.add_argument('--url', help='URL to open  (for Minibrowser)')
+    return argParser
+
+  def all_possible_build_options(self):
+    options = ['--disable-threaded-compositor', '--disable-gst-gl', '--use-opengles']
+    for L in range(0, len(options) + 1):
+        for subset in itertools.combinations(options, L):
+          yield self.get_argument_parser().parse_args(subset)
+          for_debug = ('--debug', ) + subset
+          yield self.get_argument_parser().parse_args(for_debug)
 
   def __init__(self):
     self.script_dir = os.path.dirname(os.path.realpath(__file__))
